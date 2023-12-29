9b1a38625db8d0822eaa2997223aaad624369f29
MDSAZZADSIDDIQUE <mdsazzadsiddique0@gmail.com>
2023-12-29 11:28:29 +0600

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
